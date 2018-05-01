c4a4cc37ccb40dab687429c171d0e3ca03d337e9
Lode Kennes <lode@codechamp.be>
2018-05-01 14:09:08 +0400

    fixed ts definitions as module

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,10 @@
-export default class Geocoder {
-    init(apiKey: string): void;
-    get isInit(): boolean;
-    setApiKey(API_KEY: string): void;
-    from(...params): Promise<void>;
-    getFromLocation(address: string): Promise<any>;
-    getFromLatLng(lat: number, lng: number): Promise<any>;
+declare module 'react-native-geocoding' {
+    export default class Geocoder {
+        init(apiKey: string): void;
+        get isInit(): boolean;
+        setApiKey(API_KEY: string): void;
+        from(...params): Promise<void>;
+        getFromLocation(address: string): Promise<any>;
+        getFromLatLng(lat: number, lng: number): Promise<any>;
+    }
 }
