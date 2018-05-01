a653616cea72346c40a0d2b53b86458ebdb22e43
Lode Kennes <lode@codechamp.be>
2018-05-01 14:03:06 +0400

    added ts definitions

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare class Geocoder {
+    init(apiKey: string): void;
+    get isInit(): boolean;
+    setApiKey(API_KEY: string): void;
+    from(...params): Promise<void>;
+    getFromLocation(address: string): Promise<any>;
+    getFromLatLng(lat: number, lng: number): Promise<any>;
+}
