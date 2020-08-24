c2200c3e1726b4b6fbbebea1378ee57c0eb94f2d
Brumor <paulbricout2@gmail.com>
2020-08-24 17:54:58 +0200

    Typescriptfix

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,50 @@
+declare namespace Geocoder {
+  interface GeocoderResponse {
+    results: {
+      address_components: {
+        long_name: string;
+        short_name: string;
+        types: string[];
+      }[];
+      formatted_address: string;
+      geometry: {
+        bounds: {
+          northeast: {
+            lat: number;
+            lng: number;
+          };
+          southwest: {
+            lat: number;
+            lng: number;
+          };
+        };
+        location: {
+          lat: number;
+          lng: number;
+        };
+        location_type: 'APPROXIMATE';
+        viewport: {
+          northeast: {
+            lat: number;
+            lng: number;
+          };
+          southwest: {
+            lat: number;
+            lng: number;
+          };
+        };
+      };
+      place_id: string;
+      types: string[];
+    }[];
+    status: 'OK' | string;
+  }
+
+  function init(apiKey: string, options?: Object): void;
+  function isInit(): boolean;
+  function from(...params: any[]): Promise<GeocoderResponse>;
+}
+
 declare module 'react-native-geocoding' {
-    export default class Geocoder {
-        init(apiKey: string, options: Object): void;
-        isInit(): boolean;
-        setApiKey(API_KEY: string): void;
-        from(...params: any[]): Promise<void>;
-        getFromLocation(address: string): Promise<any>;
-        getFromLatLng(lat: number, lng: number): Promise<any>;
-    }
+  export default Geocoder;
 }
